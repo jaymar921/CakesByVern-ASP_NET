985d8bed90c49e47eddd715b6b4adc3b545f31e0
JayMar921 <jayharronabejar@gmail.com>
2023-01-23 21:54:18 +0800

    Add project files.

---
+++ CakesByVern-Data/Entity/User.cs
@@ -0,0 +1,25 @@
+﻿using CakesByVern_Data.Security;
+
+namespace CakesByVern_Data.Entity
+{
+    public class User
+    {
+        public string Id { get; private set; }  
+        public string FullName { get; private set; }
+        public DateTime Birthdate { get; private set; }
+        public string Email { get; private set; }
+        public Credential Credential { get; private set; }
+
+        public User(string id, string fullname, DateTime birthdate, string email, Credential credential)
+        {
+            Id = id;
+            FullName = fullname;
+            Birthdate = birthdate;
+            Email = email;
+            Credential = credential;
+        }
+        public static User Create(string id, string fullname, DateTime birthdate, string email, Credential credential) {
+            return new User(id, fullname, birthdate, email, credential);
+        }
+    }
+}
---
+++ CakesByVern-Data/Security/Credential.cs
@@ -0,0 +1,17 @@
+﻿namespace CakesByVern_Data.Security
+{
+    public class Credential
+    {
+        public string UserName { get; private set; } = string.Empty;
+        public string Password { get; private set; } = string.Empty;
+        public Guid CredentialId { get; private set; } = Guid.NewGuid();
+
+        public Credential(Guid uid, string username, string password) 
+        { 
+            CredentialId = uid;
+            UserName = username;
+            Password = password;
+        }
+
+    }
+}
